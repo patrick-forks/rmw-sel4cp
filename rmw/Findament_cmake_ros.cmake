bdd231ba4e65c44b23da8db88e1962881b6a27ab
Patrick Hao <patrick.hao95@gmail.com>
2023-07-12 15:52:27 +1000

    Ensuring rmw can build ok.

---
+++ rmw/Findament_cmake_ros.cmake
@@ -0,0 +1,26 @@
+
+# Stubs for ament_cmake_ros.
+
+function(ament_target_dependencies)
+endfunction()
+
+function(ament_export_dependencies)
+endfunction()
+
+function(ament_export_include_directories)
+endfunction()
+
+function(ament_export_libraries)
+endfunction()
+
+function(ament_export_targets)
+endfunction()
+
+function(ament_index_register_resource)
+endfunction()
+
+function(ament_package)
+endfunction()
+
+function(ament_export_development_version_if_higher_than_manifest)
+endfunction()
